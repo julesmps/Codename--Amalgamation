082423f8013a33a3bf35299d0e2bc335ad837dbc
Kyle Chickering <kylechickering@yahoo.com>
2015-07-18 21:56:11 -0600

    added random script and random comment

---
+++ RandomCSharpScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomCSharpScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+			// Some random comment
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
