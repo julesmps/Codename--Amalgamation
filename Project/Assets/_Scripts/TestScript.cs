c52eff4eba8c594e6491ce5d78bbfec35e67aa6b
Kyle Chickering <kylechickering@yahoo.com>
2015-07-18 21:09:47 -0600

    added test to TestScript

--- Project/Assets/_Scripts/TestScript.cs
+++ Project/Assets/_Scripts/TestScript.cs
@@ -7,6 +7,8 @@ public class TestScript : MonoBehaviour {
 	void Start () {
 		// Making sure that updates to Unity Scripts
 		// are reflected in git repo
+
+		// another test
 	}
 	
 	// Update is called once per frame
