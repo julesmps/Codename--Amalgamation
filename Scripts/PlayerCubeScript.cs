b43ceddb33b4fda4a84a1339fa512dd7377001fc
Kyle Chickering <kylechickering@yahoo.com>
2015-07-19 11:50:23 -0600

    changed movement physics

--- Scripts/PlayerCubeScript.cs
+++ Scripts/PlayerCubeScript.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class PlayerCubeScript : MonoBehaviour {
 
-	// Gets rigidbody to apply forces
-	public Rigidbody player;
-
 	// Base speed for the player
 	public float speed = 100.0f;
 
 	//Base jump power for the player
 	public float jumpPower = 5.0f;
+
+	// Gets rigidbody to apply forces
+	private Rigidbody player;
 	
 	void Start () {
 
@@ -51,12 +51,11 @@ public class PlayerCubeScript : MonoBehaviour {
 		// Sets translations on the ground
 		float translation_h = Input.GetAxis ("Horizontal") * speed;
 		translation_h *= Time.deltaTime;
-
 		float translation_v = Input.GetAxis ("Vertical") * speed;
 		translation_v *= Time.deltaTime;
 
 		// Adds translation as a force to the player
-		Vector3 movement = new Vector3 (translation_h, 0.0f, translation_v);
-		player.AddForce(movement);
+		Vector3 translate = new Vector3 (translation_h, 0.0f, translation_v);
+		transform.Translate (translate);
 	}
 }
