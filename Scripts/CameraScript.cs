487a909b834992c9c57a70ad827ce18a412ae670
Ignacio Pacheco <baxter.ignalb@gmail.com>
2015-07-19 01:18:27 -0400

    Change to CameraScript
    Added height and angle so it wouldn't be a perfectly sideways view.

--- Scripts/CameraScript.cs
+++ Scripts/CameraScript.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraScript : MonoBehaviour {
 
 	const float distance = 10.0f;
+	float height = distance * 0.2f;
 
 	public GameObject player;
 
@@ -18,10 +19,11 @@ public class CameraScript : MonoBehaviour {
 	}
 
 	void position (GameObject target){
-		transform.position = new Vector3 (target.transform.position.x, target.transform.position.y, target.transform.position.z - distance);
+		transform.position = new Vector3 (0, 0, target.transform.position.z - distance);
+		transform.Rotate(new Vector3(5, 0, 0));
 	}
 
 	void moveCamera () {
-		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, transform.position.z);
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + height, transform.position.z);
 	}
 }
