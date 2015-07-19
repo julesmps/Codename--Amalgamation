1183e367df343adbda7114948025f4faef9420fa
Kyle Chickering <kylechickering@yahoo.com>
2015-07-18 23:27:59 -0600

    Updated player controls to include a jump

--- Scripts/PlayerCubeScript.cs
+++ Scripts/PlayerCubeScript.cs
@@ -7,11 +7,11 @@ public class PlayerCubeScript : MonoBehaviour {
 	public Rigidbody player;
 
 	// Base speed for the player
-	public float speed = 100.0F;
+	public float speed = 100.0f;
 
 	//Base jump power for the player
-	public float jumpPower = 10.0F;
-
+	public float jumpPower = 5.0f;
+	
 	void Start () {
 
 		// Get player rigidbody component
@@ -21,6 +21,10 @@ public class PlayerCubeScript : MonoBehaviour {
 
 	void FixedUpdate () {
 
+		// If jump key than jump
+		if (Input.GetKey ("space"))
+			player.velocity = new Vector2(0, jumpPower);
+
 		// Sets translations on the ground
 		float translation_h = Input.GetAxis ("Horizontal") * speed;
 		translation_h *= Time.deltaTime;
@@ -28,12 +32,8 @@ public class PlayerCubeScript : MonoBehaviour {
 		float translation_v = Input.GetAxis ("Vertical") * speed;
 		translation_v *= Time.deltaTime;
 
-		float jump = Input.GetAxis ("Jump") * jumpPower;
-		jump *= Time.deltaTime;
-
 		// Adds translation as a force to the player
-		Vector3 movement = new Vector3 (translation_h, 0.0F, translation_v);
+		Vector3 movement = new Vector3 (translation_h, 0.0f, translation_v);
 		player.AddForce(movement);
-	
 	}
 }
