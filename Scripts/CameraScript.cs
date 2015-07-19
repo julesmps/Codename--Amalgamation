eb5c2e7b158ac51a5169744d5bb5add015dedb28
Kyle Chickering <kylechickering@yahoo.com>
2015-07-19 12:42:18 -0600

    re-wrote and re-implemented physics, function remains constant

--- Scripts/CameraScript.cs
+++ Scripts/CameraScript.cs
@@ -5,26 +5,15 @@ public class CameraScript : MonoBehaviour {
 
 	// Changed to public
 	public const float distance = 10.0f;
-	public float height = distance * 0.8f;
-
 	public GameObject player;
 
-	// Use this for initialization
 	void Start () {
-		position(player);
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, player.transform.position.z - distance);
+		transform.Rotate(new Vector3(5.0f, 0.0f, 0.0f));
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		moveCamera ();
-	}
-
-	void position (GameObject target){
-		transform.position = new Vector3 (0, 0, target.transform.position.z - distance);
-		transform.Rotate(new Vector3(5, 0, 0));
-	}
-
-	void moveCamera () {
-		transform.position = new Vector3 (player.transform.position.x, (player.transform.position.y + height)/3, transform.position.z);
+		float height = player.transform.position.y * 0.2f;
+		transform.position = new Vector3 (player.transform.position.x, (player.transform.position.y + (height / 2)), transform.position.z);
 	}
 }
