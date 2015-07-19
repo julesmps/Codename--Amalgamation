ecae98b4705f1b623487588712ebdec2623e3ffe
Ignacio Pacheco <baxter.ignalb@gmail.com>
2015-07-19 00:08:48 -0400

    Edit to RandomScript
    Added the welcome function and its implementation, mainly for testing
    purposes.

--- Scripts/RandomCSharpScript.cs
+++ Scripts/RandomCSharpScript.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class RandomCSharpScript : MonoBehaviour {
 
+	string gameName;
+
 	// Use this for initialization
 	void Start () {
-	
-			// Some random comment
+		// Some random comment
+		welcome (gameName);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void welcome (string name){
+		if (name == null) {
+			name = "[insert name here]";
+		}
+		Debug.Log ("Welcome to the development of " + name + ".");
 	}
 }
