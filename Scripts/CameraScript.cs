a9ef5e2b8db8d542b8f2e99c5c30211e6ae3d281
Ignacio Pacheco <baxter.ignalb@gmail.com>
2015-07-19 01:08:49 -0400

    Added CameraScript
    Makes the camera move with the player

---
+++ Scripts/CameraScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	const float distance = 10.0f;
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		position(player);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveCamera ();
+	}
+
+	void position (GameObject target){
+		transform.position = new Vector3 (target.transform.position.x, target.transform.position.y, target.transform.position.z - distance);
+	}
+
+	void moveCamera () {
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, transform.position.z);
+	}
+}
