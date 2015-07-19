fdae26d567b340c4f40e3f0bb4fe7e92239f5e39
Kyle Chickering <kylechickering@yahoo.com>
2015-07-18 22:50:31 -0600

    Added new scene and player movement script

---
+++ Scripts/PlayerCubeScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCubeScript : MonoBehaviour {
+
+	// Gets rigidbody to apply forces
+	public Rigidbody player;
+
+	// Base speed for the player
+	private float movementSpeed = 100.0F;
+
+	void Start () {
+
+		// Get player rigidbody component
+		player = GetComponent<Rigidbody> ();
+	
+	}
+
+	void FixedUpdate () {
+
+		// Sets translation_h to the axis that the player has specified
+		float translation_h = Input.GetAxis ("Horizontal") * movementSpeed;
+		translation_h *= Time.deltaTime;
+
+		// Adds translation as a force to the player
+		Vector3 movement = new Vector3 (translation_h, translation_h, 0.0F);
+		player.AddForce(movement);
+	
+	}
+}
