f46a8c316f5aac3d4fd1ebc531e1549e53f9333d
Kyle Chickering <kylechickering@yahoo.com>
2015-07-20 17:33:23 -0600

    added base item class

---
+++ Scripts/Item.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+	
+	protected string itemName;
+	protected string itemDescription;
+	protected int itemID; // Item's game id
+
+	protected GameObject itemObj;
+	protected Rigidbody itemRigidbody;
+
+	void Start() {
+		Debug.Log ("item start");
+	}
+
+	/*
+	void createItem (string name,
+	                 string description,
+	                 string id
+	                 ) 
+	{
+		itemName = name;
+		itemDescription = description;
+		itemID = id;
+
+		itemObj = gameObject;
+		itemRigidbody = GetComponent<Rigidbody>;
+	}
+	*/
+
+}
