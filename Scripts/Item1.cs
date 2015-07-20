df38fb7e45d79674a37ece9a5c54cd234f41d6da
Kyle Chickering <kylechickering@yahoo.com>
2015-07-20 17:34:01 -0600

    added class for item 1

---
+++ Scripts/Item1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item1 : Item {
+	
+	void Start () {
+		itemName = "sphere";
+		itemDescription = "this object is a sphere";
+		itemID = 1;
+
+		itemRigidbody = GetComponent<Rigidbody>();
+	}
+
+	void OnCollisionEnter() {
+		itemRigidbody.velocity = new Vector2 (0.0f, 2.0f);
+	}
+}
