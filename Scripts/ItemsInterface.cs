ab55ea69a69640c82b7b477bceaebbb85edd94d4
Kyle Chickering <kylechickering@yahoo.com>
2015-07-21 00:27:38 -0600

    added item handling

---
+++ Scripts/ItemsInterface.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface ItemsInterface {
+
+	int ItemID { get; set; }
+	string ItemName { get; set; }
+	string Description { get; set; }
+}
---
+++ Scripts/ItemsParent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item {
+
+	public int id { get; set; }
+	public string name { get; set; }
+	public string description { get; set; }
+}
