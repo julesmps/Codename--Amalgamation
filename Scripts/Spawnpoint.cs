90eacbf70a5511179f9b85b4a4147b55c41ca0d4
manugovea <manugovea96@gmail.com>
2015-07-22 17:46:34 -0400

    Scripts
    to setup the patrol enemy script you need at least two waypoints and the
    enemy object. it should work for any of the 3 axis

---
+++ Scripts/Spawnpoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawnpoint : MonoBehaviour {
+
+	public GameObject Player;
+
+	//use this at start
+	void Awake(){
+		Player = GameObject.FindGameObjectWithTag ("Player"); // Make this find the player
+		SpawnPlayer();
+	}
+	
+	void SpawnPlayer(){
+		Player.transform.position = transform.position; // this calls the player to the spawnpoint
+		this.enabled = false; // this ends the function after player has spawned
+	}
+}
