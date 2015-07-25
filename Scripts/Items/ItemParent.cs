541e8f5e5aa988cb47a66c1c5c6ca9796ac54747
Kyle Chickering <kylechickering@yahoo.com>
2015-07-24 19:28:07 -0600

    updated item handling

---
+++ Scripts/Items/ItemParent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Item {
+	public class ItemParent {
+
+		private ItemType _itemType;
+
+		private string _itemName;
+		private string _itemDescription;
+		private int _itemNumber;
+
+		private GameObject _itemGameObject;
+		private Sprite _itemSprite;
+
+
+	}
+}
---
+++ Scripts/Items/ItemType.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Item {
+	public enum ItemEnums {
+		basic,
+		weapon,
+		food
+	}
+}
