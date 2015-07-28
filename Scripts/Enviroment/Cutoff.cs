edec87cb194cb250239895f40173df493395c87c
manugovea <manugovea96@gmail.com>
2015-07-28 12:23:39 -0400

    cutoff updated

--- Scripts/Enviroment/Cutoff.cs
+++ Scripts/Enviroment/Cutoff.cs
@@ -5,8 +5,8 @@ public class Cutoff : MonoBehaviour {
 	
 /*
 to set up the cutoff set the script to an empty game object and set it
-at the points where you dont wnat your player to go pass. to set this 
-as an altitude cutoff just change the x axis in the if statement for a y.
+at the points where you dont wnat your player to go pass. you have to 
+set up one for each of the z planes
 */
 
 	public GameObject Player;
@@ -16,10 +16,12 @@ as an altitude cutoff just change the x axis in the if statement for a y.
 		Player = GameObject.FindGameObjectWithTag ("Player"); // make this find the Player
 	}
 
-	void Update(){ //if the x coordinate of player position is less or equal to the x of this point
-		if (Player.transform.position.x <= transform.position.x) {
-			Player.transform.position = transform.position; /*then the possition of the player equals the 
+	void Update(){ 
+		if (Player.transform.position.z == transform.position.z) { //if the z coordinate of player position is equal to the z of this point
+			if (Player.transform.position.x <= transform.position.x) { //if the x coordinate of player position is less or equal to the x of this point
+				Player.transform.position = transform.position; /*then the possition of the player equals the 
 			                                                  possition of point*/
+			}
 		}
 	}
 }
