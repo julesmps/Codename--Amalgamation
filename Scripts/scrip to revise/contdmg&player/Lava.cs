995fa113d2dd1f12449e23fa06bc18aa401e8348
manugovea <manugovea96@gmail.com>
2015-10-06 20:12:44 -0400

    lava script and a basic player GUI to tryout the code

---
+++ Scripts/scrip to revise/contdmg&player/Lava.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lava : MonoBehaviour {
+
+/*
+Add this script to a block with a trigger and make sure you put something under it so the player doesnt fall through
+you also need to apply the script "Player" to the playable character so it can take life points from it.
+	 */
+
+	public GameObject player;
+	public GameObject lava;
+	public float lavaDmg = 0;
+	public float lavaDmgCldwn = 1;
+	public int lavaCldwn = 5;
+	public bool triggerOn = false;
+	public bool whileOnTrigger = false;
+	
+	void Awake (){
+		player = GameObject.FindGameObjectWithTag ("Player");
+		lava = GameObject.FindGameObjectWithTag ("Lava");
+	}
+
+	//recognice that the player is inside the trigger
+	void OnTriggerStay (Collider c){
+		if (c.gameObject.tag == "Player") {
+			whileOnTrigger = true;
+		}
+	}
+
+	//recognice if the player left the trigger
+	void OnTriggerExit (Collider c){
+		if (c.gameObject.tag == "Player"){
+			whileOnTrigger = false;
+			triggerOn = true;
+		}
+	}
+
+	void Update (){
+		//call for variables in the script "Player"
+		Player pstats = player.GetComponent<Player>();
+
+		//continous damage while on trigger
+		if (whileOnTrigger == true) {
+			lavaDmg = 5;
+
+			if (lavaDmgCldwn <= 1){
+				lavaDmgCldwn -= Time.deltaTime;
+			}
+			if (lavaDmgCldwn <= 0){
+				pstats.CurHp -= lavaDmg;
+				lavaDmgCldwn = 1;
+			}
+		}
+
+		//count to stop the damage after player leaves trigger
+		if (triggerOn == true){
+			lavaDmg = 5;
+					 
+			if (lavaDmgCldwn <= 1){
+				lavaDmgCldwn -= Time.deltaTime;
+			}
+			if (lavaDmgCldwn <= 0){
+				pstats.CurHp -= lavaDmg;
+				lavaCldwn -= 1;
+				lavaDmgCldwn = 1;
+			}
+			if (lavaCldwn <= 0){
+				lavaDmg = 0;
+				triggerOn = false;
+				lavaCldwn = 5;
+			}
+		}
+	}
+}
---
+++ Scripts/scrip to revise/contdmg&player/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+//Basic GUI for player with a hp value and its hp bar to test the contiouos damage script "lava"
+//Attach to the player, and give it the tag Player
+	public GameObject player;
+	public GameObject spawn;
+
+	// hp related variables
+	public float MaxHp = 200;
+	public float CurHp = 200;
+	public int PlayerLives = 5;
+	//End
+
+	// GUI variables
+	public GUIStyle BlackBar;
+	public GUIStyle HpBar;
+	public GUIStyle Textfield;
+	//End
+
+	void Awake (){
+		player = GameObject.FindGameObjectWithTag ("Player");
+		spawn = GameObject.FindGameObjectWithTag ("Spawn");
+	}
+
+	void Update (){
+		if (CurHp <= 0) {
+			CurHp = 0;
+		}
+		if (CurHp >= MaxHp){
+			CurHp = MaxHp;
+		}
+	}
+
+	void OnGUI (){
+	  	
+		//health bar
+		GUI.Box (new Rect (20, 5, 200, 25), "", BlackBar); //black bar behind the hpbar
+		GUI.Box (new Rect (20, 5, CurHp, 25), "", HpBar); // hp bar
+		GUI.Box (new Rect (25, 10, 200, 25), "HP: " + CurHp + "/" + MaxHp, Textfield);
+
+	}
+
+
+}
